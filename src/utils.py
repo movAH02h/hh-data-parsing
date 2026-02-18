c20a3ac19df15ca42c8dc9aa36f9459a8302e5fe
krum <122667404+movAH02h@users.noreply.github.com>
2026-02-18 21:11:50 +0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,29 @@
+import os
+import logging
 
+logger = logging.getLogger(__name__)
+
+def get_user_file_path(default_filename="hh.csv") -> Optional[Tuple[str, str]]:
+    """
+    Запрашивает путь к .csv файлу.
+
+    Аргументы:
+        Нет.
+
+    Вернёт:
+        Optional[str]: путь_к_файлу в случае успеха, иначе None.
+
+    Исключения:
+        Ошибки ввода-вывода при обращении к файловой системе.
+    """
+    path = input("Введите путь к исходному файлу: ").strip()
+
+    if not path:
+      base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+      path_csv = os.path.join(base_dir, "data", default_filename)
+
+    if not os.path.exists(path):
+        logger.error(f"Файл '{path}' не найден")
+        return None
+
+    return path
